f69fc060a8f5849c47448ddb90521792a74afc75
陈碧松 <sdfcbs91@163.com>
2015-02-11 11:00:28 +0800

    四个格子拖拽设置大小
    第二版,布局采用与第一版不同的绝对定位,算法不同，加上了最小高度、宽度过滤

---
+++ demo.js
@@ -0,0 +1,197 @@
+﻿var demo = function (option) {
+    this.advanceConfig = {
+        upper: this,
+        layerContrl: null,
+        control: null,
+        width: 0,
+        height: 0,
+        //高级编辑入口
+        create: function () {
+            var self = this;
+            self.width = $(window).width(), self.height = $(window).height();
+            self.layerContrl = common.alert.create({
+                width: self.width, height: self.height, control: "<div class='advanceConfig' id='advanceConfig'>" +
+                    "<div class='boxs fieldConfig'><div class='dragWResize'></div><div class='dragSResize'></div><div class='dragSeResize' style='bottom:0;right:0;'><div class='moveIcon'></div></div><div class='line'><div class='fl'>配置窗口</div></div><div class='line'><div class='tabRect' config='basic'></div><div class='tabRect ml15' config='axisX'></div><div class='tabRect ml15' config='axisY'></div><div class='tabRect ml15' config='picType'></div></div></div>" +
+                    "<div class='boxs preview'><div class='dragWResize'></div><div class='dragSResize'></div><div class='dragNeResize' style='bottom:0;left:0;'><div class='moveIcon'></div></div></div>" +
+                    "<div class='boxs dataConfig'><div class='dragWResize'></div><div class='dragSResize'></div><div class='dragNeResize' style='top:0;right:0;'><div class='moveIcon'></div></div></div>" +
+                    "<div class='boxs sqlConfig'><div class='dragWResize'></div><div class='dragSResize'></div><div class='dragSeResize' style='top:0;left:0;'><div class='moveIcon'></div></div></div>" +
+                    "</div>",
+                showClose: false, loaded: function () {
+                    self.set();
+                },
+                onresize: function () {
+                    self.width = $(window).width(), self.height = $(window).height();
+                    self.layerContrl.$content.width(self.width).height(self.height).css({ "top": "0px", "left": "0px" });
+                    self.setPosition();
+                },
+                close: function () {
+                    self.layerContrl = null;
+                }
+            });
+        },
+
+        set: function () {
+            var advanceJson = {},
+                upper = this.upper,
+                dataConfig = upper.config || {},
+                advance = dataConfig.AdvanceConfig || "",
+                u = upper.control = {},
+                $advanceConfig = u.$advanceConfig = $("#advanceConfig");
+
+            u.$boxs = $advanceConfig.children(".boxs");
+            var $fieldConfig = u.$fieldConfig = $advanceConfig.children(".fieldConfig"),
+                $preview = u.$preview = $advanceConfig.children(".preview"),
+                $dataConfig = u.$dataConfig = $advanceConfig.children(".dataConfig"),
+                $sqlConfig = u.$sqlConfig = $advanceConfig.children(".sqlConfig");
+            u.$basic = $fieldConfig.find("div[config='basic']"),
+            u.$axisX = $fieldConfig.find("div[config='axisX']"),
+            u.$axisY = $fieldConfig.find("div[config='axisY']"),
+            u.$picType = $fieldConfig.find("div[config='picType']");
+
+            try {
+                advanceJson = JSON.parse(advance);
+            } catch (e) {
+            }
+
+            this.initHtml();
+            //this.setPosition();
+            this.setData();
+
+            //girdResize
+            var g = new girdResize([
+                {
+                    drag: [{ control: $fieldConfig.children(".dragSResize"), type: "s" }, { control: $fieldConfig.children(".dragWResize"), type: "w" }, { control: $fieldConfig.children(".dragSeResize"), type: "sw"}],
+                    control: $fieldConfig,
+                    location: "left_top"
+                },
+
+                {
+                    drag: [{ control: $preview.children(".dragSResize"), type: "s" }, { control: $preview.children(".dragWResize"), type: "w" }, { control: $preview.children(".dragNeResize"), type: "sw"}],
+                    control: $preview,
+                    location: "right_top"
+                },
+                {
+                    drag: [{ control: $dataConfig.children(".dragSResize"), type: "s" }, { control: $dataConfig.children(".dragWResize"), type: "w" }, { control: $dataConfig.children(".dragNeResize"), type: "sw"}],
+                    control: $dataConfig,
+                    location: "left_bottom"
+                },
+
+                {
+                    drag: [{ control: $sqlConfig.children(".dragSResize"), type: "s" }, { control: $sqlConfig.children(".dragWResize"), type: "w" }, { control: $sqlConfig.children(".dragSeResize"), type: "sw"}],
+                    control: $sqlConfig,
+                    location: "right_bottom"
+                },
+                { event: {
+                    mousedown: function () { },
+                    mouseup: function (gridResize) {
+                        var g = gridResize; //this.eachOptions
+                        g.eachOptions(function (option) {
+                            console.log(option)
+                        });
+                    }
+                }
+                }
+            ]);
+        },
+        initHtml: function () {
+            this.initFieldConfigHtml();
+        },
+        setPosition: function () {
+            var u = this.upper.control;
+            var $boxs = u.$advanceConfig.children(".boxs");
+            var w = this.width,
+                h = this.height,
+                boxPadW = parseInt($boxs.css("padding-left").replace("px", "")) * 2,
+                boxPadH = parseInt($boxs.css("padding-top").replace("px", "")) * 2,
+                boxW = (w - 10) / 2 - 30 - boxPadW, //(w - 10 - 60 - boxPadW) / 2,
+                boxH = (h - 10) / 2 - 30 - boxPadH;
+            $boxs.width(boxW).height(boxH);
+
+            this.setFieldConfigPosition(boxW, boxH, boxPadW, boxPadH);
+            this.setPreviewPosition(boxW, boxH, boxPadW, boxPadH);
+            this.setDataConfigPosition(boxW, boxH, boxPadW, boxPadH);
+            this.setSqlConfigPosition(boxW, boxH, boxPadW, boxPadH);
+        },
+        setData: function () {
+        },
+        initFieldConfigHtml: function () {
+            var u = this.upper.control;
+
+            u.$basic.append("<div class='title'><span class='num'>1</span><span class='tit'>基本配置</span></div>");
+            u.$axisX.append("<div>X轴配置</div>");
+            u.$axisY.append("<div>Y轴配置</div>");
+            u.$picType.append("<div>线型配置</div>");
+        },
+        setFieldConfigPosition: function (boxW, boxH, boxPadW, boxPadH) {
+            var u = this.upper.control,
+                $fieldConfig = u.$fieldConfig,
+                $tabRect = $fieldConfig.find('.tabRect'),
+                $dragWResize = $fieldConfig.find('.dragWResize'),
+                $dragSResize = $fieldConfig.find('.dragSResize'),
+                $dragSeResize = $fieldConfig.find('.dragSeResize'),
+                sHeight = $dragSResize.height(),
+                seWidth = $dragSeResize.width(),
+                seHeight = $dragSeResize.height(),
+                w = boxW + boxPadW,
+                h = boxH + boxPadH,
+                tabRectW = (boxW - 15 * 3 - 2 * 4 - 2) / 4,
+                tabRectH = boxH - 30 - 10;
+
+            $tabRect.width(tabRectW).height(tabRectH);
+            $dragWResize.height(h - seHeight).css({ "top": "0", "right": "0" });
+            $dragSResize.width(w - seWidth).css({ "bottom": "0", "left": "0" });
+            $dragSeResize.css({ "bottom": "0", "right": "0" });
+        },
+        setPreviewPosition: function (boxW, boxH, boxPadW, boxPadH) {
+            var u = this.upper.control,
+                $preview = u.$preview,
+                $tabRect = $preview.find('.tabRect'),
+                $dragWResize = $preview.find('.dragWResize'),
+                $dragSResize = $preview.find('.dragSResize'),
+                $dragNeResize = $preview.find('.dragNeResize'),
+                sHeight = $dragSResize.height(),
+                seWidth = $dragNeResize.width(),
+                seHeight = $dragNeResize.height(),
+                w = boxW + boxPadW,
+                h = boxH + boxPadH;
+            $dragWResize.height(h - seHeight).css({ "top": "0", "left": "0" });
+            $dragSResize.width(w - seWidth).css({ "bottom": "0", "right": "0" });
+            $dragNeResize.css({ "bottom": "0", "left": "0" });
+        },
+        setDataConfigPosition: function (boxW, boxH, boxPadW, boxPadH) {
+            var u = this.upper.control,
+                $dataConfig = u.$dataConfig,
+                $tabRect = $dataConfig.find('.tabRect'),
+                $dragWResize = $dataConfig.find('.dragWResize'),
+                $dragSResize = $dataConfig.find('.dragSResize'),
+                $dragNeResize = $dataConfig.find('.dragNeResize'),
+                sHeight = $dragSResize.height(),
+                seWidth = $dragNeResize.width(),
+                seHeight = $dragNeResize.height(),
+                w = boxW + boxPadW,
+                h = boxH + boxPadH;
+            $dragWResize.height(h - seHeight).css({ "bottom": "0", "right": "0" });
+            $dragSResize.width(w - seWidth).css({ "top": "0", "left": "0" });
+            $dragNeResize.css({ "top": "0", "right": "0" });
+        },
+        setSqlConfigPosition: function (boxW, boxH, boxPadW, boxPadH) {
+            var u = this.upper.control,
+                $dataConfig = u.$sqlConfig,
+                $tabRect = $dataConfig.find('.tabRect'),
+                $dragWResize = $dataConfig.find('.dragWResize'),
+                $dragSResize = $dataConfig.find('.dragSResize'),
+                $dragSeResize = $dataConfig.find('.dragSeResize'),
+                sHeight = $dragSResize.height(),
+                seWidth = $dragSeResize.width(),
+                seHeight = $dragSeResize.height(),
+                w = boxW + boxPadW,
+                h = boxH + boxPadH;
+            $dragWResize.height(h - seHeight).css({ "bottom": "0", "left": "0" });
+            $dragSResize.width(w - seWidth).css({ "top": "0", "right": "0" });
+            $dragSeResize.css({ "top": "0", "left": "0" });
+        },
+        get: function () {
+            var advanceJson = {}, upper = this.upper, dataConfig = upper.config;
+        }
+    }
+}
---
+++ drag.js
@@ -0,0 +1,136 @@
+/**
+**参数:[{drag:{control:"",type:"s"},control:"",location:"left_top"}]
+**/
+var girdResize = function (o) {
+    this.configs = o;
+    this.getWidth = function () { this.width = parseInt($(window).width() + $(window.document).scrollLeft()), this.paddingW = parseInt(this.width / 100); };
+    this.getHeight = function () { this.height = parseInt($(window).height() + $(window.document).scrollTop()), this.paddingH = parseInt(this.height / 50); };
+    this.setDefault = function () { this.blag = { drag: { width: true, height: true}} };
+    this.start = function () {
+        this.getWidth();
+        this.getHeight();
+        this.setDefault();
+        position = { left: parseInt(this.width / 2), top: parseInt(this.height / 2) };
+        this.position(position);
+        this.event();
+    }
+    this.event = function () {
+        var self = this;
+        this.each(function (config) {
+            //具备元素和类型,才有意义定义其拖拽方式
+            if (config.drag) {
+                var d = config.drag;
+                for (var i = 0; i < d.length; i++) {
+                    if (d[i].control && d[i].type) {
+                        if (d[i].type in self.move) {
+                            d[i].control = $(d[i].control);
+                            self.move[d[i].type](d[i].control);
+                        }
+                    }
+                }
+            }
+        });
+    }
+    this.position = function (position) {
+        var self = this;
+        this.each(function (config) {
+            //具备元素和定位,才有意义帮助其定位
+            if (config.control && config.location) {
+                if (config.location in self) {
+                    if (typeof config.control !== "object") config.control = $(config.control);
+                    
+                    var size = self[config.location](position), control = config.control, blag = self.blag.drag;
+                    if (blag.width) {
+                        control.width(size.width)[0].style.left = size.left
+                    }
+                    if (blag.height) {
+                        control.height(size.height)[0].style.top = size.top
+                    }
+
+                }
+            }
+        });
+    }
+    //验证元素是否继续按照鼠标点移动定位和改变大小
+    this.matchPosition = function (position) {
+        var self = this, re = true;
+        this.each(function (config) {
+            //具备元素和定位,才有意义帮助其定位
+            if (config.control && config.location) {
+                if (config.location in self) {
+                    var size = self[config.location](position), blag = self.blag.drag;
+                    if (size.width < 300) {
+                        blag.width = false;
+                    }
+                    if (size.height < 100) {
+                        blag.height = false;
+                    }
+                    if (!blag.width && !blag.height) {
+                        re = false;
+                        return false;
+                    }
+                }
+            }
+        });
+        return re;
+    }
+    this.each = function (callback) {
+        var arr = this.configs;
+        for (var i = 0; i < arr.length; i++) {
+            var c = callback(arr[i]);
+            if (typeof c === "boolean") {
+                if (c === false) break;
+            }
+        }
+    }
+    this.left_top = function (position) {
+        return { top: this.paddingH + "px", left: this.paddingW + "px", width: position.left - parseInt(1.5 * this.paddingW), height: position.top - parseInt(1.5 * this.paddingH) };
+    }
+    this.right_top = function (position) {
+        return { top: this.paddingH + "px", left: position.left + parseInt(0.5 * this.paddingW) + "px", width: this.width - position.left - parseInt(1.5 * this.paddingW), height: position.top - parseInt(1.5 * this.paddingH) };
+    }
+    this.left_bottom = function (position) {
+        return { top: position.top + parseInt(0.5 * this.paddingH) + "px", left: this.paddingW + "px", width: position.left - parseInt(1.5 * this.paddingW), height: this.height - position.top - parseInt(1.5 * this.paddingH) };
+    }
+    this.right_bottom = function (position) {
+        return { top: position.top + parseInt(0.5 * this.paddingH) + "px", left: position.left + parseInt(0.5 * this.paddingW) + "px", width: this.width - position.left - parseInt(1.5 * this.paddingW), height: this.height - position.top - parseInt(1.5 * this.paddingH) };
+    }
+    this.getCenterPosition = function () {
+        var re = { top: 0, left: 0 };
+        this.each(function (config) {
+            if (config.location === "left_top") {
+                var control = config.control;
+                re.top = control.height() + parseInt(control[0].style.top.replace("px", "")) * 1.5;
+                re.left = control.width() + parseInt(control[0].style.left.replace("px", "")) * 1.5;
+                return false;
+            }
+        });
+        return re;
+    }
+    this.move = {
+        upper: this,
+        sw: function (control) {
+            var self = this, upper = this.upper, blag = upper.blag.drag;
+
+            control.unbind("mousedown").mousedown(function () {
+                var cPosition = upper.getCenterPosition();
+                var dif = { left: NowMousePosition[0] - cPosition.left, top: NowMousePosition[1] - cPosition.top }
+                AddMouseMoveFunc("mouseMove_gridResize", function () {
+                    var position = { left: NowMousePosition[0] - dif.left, top: NowMousePosition[1] - dif.top };
+                    //是否符合变化元素大小条件
+                    if (upper.matchPosition(position)) {
+                        upper.position(position);
+                    }
+                    blag.width = true, blag.height = true;
+                });
+                AddMouseUpFunc("mouseUp_gridResize", function () {
+
+                    removeMMRFunc("mouseMove_gridResize");
+                    removeMURFunc("mouseUp_gridResize");
+                });
+            });
+        }
+    }
+
+    this.start();
+}
